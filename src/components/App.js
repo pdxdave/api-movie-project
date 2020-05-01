12eefeafe9bec760580ff4118fefbfb984fda7aa
pdxdave <webdevdave@hotmail.com>
2020-05-01 02:18:58 -0700

    added heroimage usehomefetch and home

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import Header from './elements/Header'
+import Home from './Home'
+
 const App = () => {
   return (
     <div>
-        Start here
+      <Header />
+      <Home />
     </div>
   );
 }
